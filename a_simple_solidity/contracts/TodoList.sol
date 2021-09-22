eed2d8da271e02a5758a74fd5dcc8f3840d85370
Tobias Leinss <caruso33@web.de>
2021-09-22 18:55:39 +0200

    Add taskCompletedToggle and test

--- a_simple_solidity/contracts/TodoList.sol
+++ a_simple_solidity/contracts/TodoList.sol
@@ -12,6 +12,8 @@ contract TodoList {
 
     event TaskCreated(uint256 id, string content, bool completed);
 
+    event taskCompleteToggled(uint256 id, bool completed);
+
     mapping(uint256 => Task) public tasks;
 
     constructor() public {
@@ -21,6 +23,15 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+
+        emit taskCompleteToggled(_id, _task.completed);
     }
 }
