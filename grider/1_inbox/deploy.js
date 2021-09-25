7713dffbdd34bc0cb12b27eb344500e61a60fabd
Tobias Leinss <caruso33@web.de>
2021-09-25 22:12:52 +0200

    Deploy inbox, finish

--- grider/1_inbox/deploy.js
+++ grider/1_inbox/deploy.js
@@ -1,34 +1,47 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require("web3")
 
-const { nodeUrl, metamask } = require('./config');
+const dotenv = require("dotenv")
+dotenv.config()
 
-// accounts number 1 !
-const provider = new HDWalletProvider(metamask, nodeUrl, 1);
-const web3 = new Web3(provider);
+const contracts = require("./compile")
 
-const INITIAL_STRING = 'Hi there!';
-let deployedContract = '',
-  accounts = '';
+const contractName = "Inbox"
+const inboxContract = contracts[contractName]
+
+const { evm, abi } = inboxContract
+
+// using https://infura.io to deploy to test / main nets
+// as connection point instead of having to set up a local
+// node and deploying it from there
+
+const provider = new HDWalletProvider(process.env.mnemonic, process.env.network)
+const web3 = new Web3(provider)
+
+const INITIAL_STRING = "Hi there!"
+let deployedContract = "",
+  accounts = ""
 
 const deploy = async () => {
   try {
-    accounts = await web3.eth.getAccounts();
+    accounts = await web3.eth.getAccounts()
 
-    console.log('Deploy from accounts', accounts[0]);
+    console.log("Deploy from accounts", accounts[0])
 
-    deployedContract = await new web3.eth.Contract(JSON.parse(interface))
+    deployedContract = await new web3.eth.Contract(abi)
       .deploy({
-        data: `0x${bytecode}`,
-        arguments: [INITIAL_STRING]
+        data: `0x${evm.bytecode.object}`,
+        arguments: [INITIAL_STRING],
       })
-      .send({ gas: '1000000', from: accounts[0] });
+      .send({ gas: "1000000", from: accounts[0] })
   } catch (e) {
-    console.log('Error while deploying', e);
+    console.log("Error while deploying", e)
   }
 
-  console.log('Contract deployed to', deployedContract.options.address);
-};
+  console.log("Contract deployed to", deployedContract.options.address)
+}
 
-deploy();
+deploy()
+
+// was deployed to 
+// 0xEf6d29dDFf75C3aC09C7AA37B3ea58aA2Bb24EB5
