6765135535e19af824e72a9b137e6aeafc6254f9
Tobias Leinss <tobias.leinss@ipa.fraunhofer.de>
2021-09-28 16:14:36 +0200

    Make react functional component

--- grider/2_lottery/client/src/App.js
+++ grider/2_lottery/client/src/App.js
@@ -1,26 +1,29 @@
-import React, { Component, Fragment } from "react"
+import React, { useEffect, useState } from "react"
 import "./App.css"
-
-import web3 from "./web3"
 import lottery from "./lottery"
+import web3 from "./web3"
 
-class App extends Component {
-  state = { manager: "", players: [], balance: "", value: "", message: "" }
+export default function App() {
+  const [manager, setManager] = useState("")
+  const [players, setPlayers] = useState([])
+  const [balance, setBalance] = useState("")
+  const [message, setMessage] = useState("")
+  const [value, setValue] = useState("")
 
-  async componentDidMount() {
-    // you don't have to specify the {from: account}-config in call() because
-    // you will use the active account from metamask (through the used metamask's provider)
+  useEffect(async () => {
     const manager = await lottery.methods.manager().call()
     const players = await lottery.methods.getPlayers().call()
     const balance = await web3.eth.getBalance(lottery.options.address)
 
-    this.setState({ manager, players, balance })
-  }
+    setManager(manager)
+    setPlayers(players)
+    setBalance(balance)
+  }, [])
 
-  onEnterSubmit = async (event) => {
+  async function onEnterSubmit(event) {
     event.preventDefault()
 
-    const floatValue = parseFloat(this.state.value)
+    const floatValue = parseFloat(value)
     if (isNaN(floatValue) || floatValue <= 0) {
       console.error("Value must be a number and greater than zero.")
       return
@@ -35,20 +38,21 @@ class App extends Component {
       )
       return
     }
-    this.setState({ message: "Waiting on transaction success..." })
+    setMessage("Waiting on transaction success...")
 
     await lottery.methods.enter().send({
       from: accounts[0],
-      value: web3.utils.toWei(this.state.value),
+      value: web3.utils.toWei(value),
     })
 
-    this.setState({ message: "You have been entered!", value: "" })
+    setMessage("You have been entered!")
+    setValue(value)
   }
 
-  onPickWinner = async (event) => {
+  const onPickWinner = async (event) => {
     event.preventDefault()
 
-    this.setState({ message: "Waiting on transaction success..." })
+    setMessage("Waiting on transaction success...")
 
     await web3.eth.requestAccounts().catch(() => {})
 
@@ -59,50 +63,43 @@ class App extends Component {
       )
       return
     }
-    console.log({ accounts })
 
     await lottery.methods.pickWinner().send({ from: accounts[0] })
 
-    this.setState({ message: "A winner has been picked!" })
+    setMessage("A winner has been picked!")
   }
 
-  render() {
-    const { manager, players, message, balance } = this.state
+  return (
+    <>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {manager}.
+        <br />
+        There are currently {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")} ether!
+      </p>
 
-    return (
-      <Fragment>
-        <h2>Lottery Contract</h2>
-        <p>
-          This contract is managed by {manager}.
-          <br />
-          There are currently {players.length} people entered, competing to win{" "}
-          {web3.utils.fromWei(balance, "ether")} ether!
-        </p>
+      <hr />
 
-        <hr />
+      <form onSubmit={onEnterSubmit} action="">
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            onChange={(event) => setValue(event.target.value)}
+            type="number"
+            step="0.001"
+          />
+          <button>Enter</button>
+        </div>
+      </form>
 
-        <form onSubmit={this.onEnterSubmit} action="">
-          <h4>Want to try your luck?</h4>
-          <div>
-            <label>Amount of ether to enter</label>
-            <input
-              onChange={(event) => this.setState({ value: event.target.value })}
-              type="number"
-              step="0.001"
-            />
-            <button>Enter</button>
-          </div>
-        </form>
+      <hr />
 
-        <hr />
-
-        <h4>Ready to pick a winner?</h4>
-        <button onClick={this.onPickWinner}>Pick a winner!</button>
-        <hr />
-        <h1>{message}</h1>
-      </Fragment>
-    )
-  }
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={onPickWinner}>Pick a winner!</button>
+      <hr />
+      <h1>{message}</h1>
+    </>
+  )
 }
-
-export default App
