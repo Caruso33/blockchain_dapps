797f86ed0fbb40586eec90cdc32bbbfb98246ee5
Tobias Leinss <tobias.leinss@ipa.fraunhofer.de>
2021-10-05 12:59:32 +0200

    Add new page

--- grider/3_kickstartR/pages/campaigns/new.js
+++ grider/3_kickstartR/pages/campaigns/new.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import Layout from '../../components/Layout';
-import { Button, Form, Input, Message } from 'semantic-ui-react';
-import factory from '../../ethereum/factory';
-import web3 from '../../ethereum/web3';
-import { Router } from '../../routes';
+import { withRouter } from "next/router"
+import React, { Component } from "react"
+import { Button, Form, Input, Message } from "semantic-ui-react"
+import Layout from "../../components/Layout"
+import getFactory from "../../ethereum/factory"
+import web3 from "../../ethereum/web3"
 
 class CampaignNew extends Component {
-  state = { minimumContribution: '', errorMessage: '', loading: false };
+  state = { minimumContribution: "", errorMessage: "", loading: false }
 
-  onSubmit = async event => {
-    event.preventDefault();
+  factory = getFactory(process.env.NEXT_PUBLIC_FACTORY_ADDRESS)
 
-    this.setState({ loading: true, errorMessage: '' });
+  onSubmit = async (event) => {
+    event.preventDefault()
+
+    this.setState({ loading: true, errorMessage: "" })
+
     try {
-      const accounts = await web3.eth.getAccounts();
-      await factory.methods
+      const accounts = await web3.eth.requestAccounts()
+
+      await this.factory.methods
         .createCampaign(this.state.minimumContribution)
         // metamask will guess the amount of gas needed
         // and will apply it to the send, so no need to do it yourself
-        .send({ from: accounts[0] });
+        .send({ from: accounts[0] })
 
-      Router.pushRoute('/');
+      this.props.router.push("/")
     } catch (e) {
-      console.error(e);
-      this.setState({ errorMessage: e.message });
+      console.error(e)
+
+      this.setState({ errorMessage: e.message })
     }
 
     this.setState({
-      loading: false
-    });
-  };
+      loading: false,
+    })
+  }
+
   render() {
     return (
       <Layout>
@@ -43,11 +49,12 @@ class CampaignNew extends Component {
               labelPosition="right"
               label="wei"
               value={this.state.minimumContribution}
-              onChange={event =>
+              onChange={(event) =>
                 this.setState({
-                  minimumContribution: event.target.value
+                  minimumContribution: event.target.value,
                 })
               }
+              type="number"
             />
           </Form.Field>
 
@@ -63,8 +70,8 @@ class CampaignNew extends Component {
           />
         </Form>
       </Layout>
-    );
+    )
   }
 }
 
-export default CampaignNew;
+export default withRouter(CampaignNew)
