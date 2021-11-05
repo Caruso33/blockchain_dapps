68ea37200099a2a078584363be0e45e8dfd174b5
Tobias Leinss <tobias.leinss@ipa.fraunhofer.de>
2021-11-05 20:49:32 +0100

    Add trading gecko starter

---
+++ trading/market_prices/src/index.js
@@ -0,0 +1,78 @@
+const db = require("./db.js")
+const { Coin } = require("./models.js")
+
+//1. Import coingecko-api
+const CoinGecko = require("coingecko-api")
+
+//2. Initiate the CoinGecko API Client
+const CoinGeckoClient = new CoinGecko()
+
+//3. Make calls
+var write = async () => {
+  const ping = await CoinGeckoClient.ping()
+
+  const pages = 1
+  const perPage = 1
+
+  const classedCoins = []
+  for (let page of Array(pages)
+    .fill()
+    .map((_x, i) => i)) {
+    const { data: coins } = await CoinGeckoClient.coins.markets({
+      vs_currency: "usd",
+      order: "market_cap_desc",
+      per_page: perPage,
+      page: page,
+    })
+
+    for (let coin of coins) {
+      let classedCoin = new Coin({
+        id: coin.id,
+        symbol: coin.symbol,
+        name: coin.name,
+        image: coin.image,
+        current_price: coin.current_price,
+        market_cap: coin.market_cap,
+        market_cap_rank: coin.market_cap_rank,
+        fully_diluted_valuation: coin.fully_diluted_valuation,
+        total_volume: coin.total_volume,
+        high_24h: coin.high_24h,
+        low_24h: coin.low_24h,
+        price_change_24h: coin.price_change_24h,
+        price_change_percentage_24h: coin.price_change_percentage_24h,
+        market_cap_change_24h: coin.market_cap_change_24h,
+        market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h,
+        circulating_supply: coin.circulating_supply,
+        total_supply: coin.total_supply,
+        max_supply: coin.max_supply,
+        ath: coin.ath,
+        ath_change_percentage: coin.ath_change_percentage,
+        ath_date: coin.ath_date,
+        atl: coin.atl,
+        atl_change_percentage: coin.atl_change_percentage,
+        atl_date: coin.atl_date,
+        roi: coin.roi,
+        last_update: coin.last_updated,
+      })
+
+      classedCoins.push(classedCoin)
+    }
+  }
+
+  for (let coin of classedCoins) {
+    db.push(`/coins/${coin.id}`, coin.serialize())
+  }
+}
+
+async function read() {
+  const coins = await db.getData("/coins")
+
+  for (let coin of Object.keys(coins)) {
+    const classedCoin = Coin.deserialize(coins[coin])
+
+    console.log(classedCoin)
+  }
+}
+
+// write()
+read()
---
+++ trading/market_prices/src/models.js
@@ -0,0 +1,150 @@
+class Coin {
+  constructor({
+    id,
+    symbol,
+    name,
+    image,
+    current_price,
+    market_cap,
+    market_cap_rank,
+    fully_diluted_valuation,
+    total_volume,
+    high_24h,
+    low_24h,
+    price_change_24h,
+    price_change_percentage_24h,
+    market_cap_change_24h,
+    market_cap_change_percentage_24h,
+    circulating_supply,
+    total_supply,
+    max_supply,
+    ath,
+    ath_change_percentage,
+    ath_date,
+    atl,
+    atl_change_percentage,
+    atl_date,
+    roi,
+    last_updated,
+  }) {
+    this.id = id
+    this.symbol = symbol
+    this.name = name
+    this.image = image
+    this.current_price = current_price
+    this.market_cap = market_cap
+    this.market_cap_rank = market_cap_rank
+    this.fully_diluted_valuation = fully_diluted_valuation
+    this.total_volume = total_volume
+    this.high_24h = high_24h
+    this.low_24h = low_24h
+    this.price_change_24h = price_change_24h
+    this.price_change_percentage_24h = price_change_percentage_24h
+    this.market_cap_change_24h = market_cap_change_24h
+    this.market_cap_change_percentage_24h = market_cap_change_percentage_24h
+    this.circulating_supply = circulating_supply
+    this.total_supply = total_supply
+    this.max_supply = max_supply
+    this.ath = ath
+    this.ath_change_percentage = ath_change_percentage
+    this.ath_date = ath_date
+    this.atl = atl
+    this.atl_change_percentage = atl_change_percentage
+    this.atl_date = atl_date
+    this.roi = roi
+    this.last_updated = last_updated
+  }
+
+  serialize() {
+    return JSON.stringify({
+      id: this.id,
+      symbol: this.symbol,
+      name: this.name,
+      image: this.image,
+      current_price: this.current_price,
+      market_cap: this.market_cap,
+      market_cap_rank: this.market_cap_rank,
+      fully_diluted_valuation: this.fully_diluted_valuation,
+      total_volume: this.total_volume,
+      high_24h: this.high_24h,
+      low_24h: this.low_24h,
+      price_change_24h: this.price_change_24h,
+      price_change_percentage_24h: this.price_change_percentage_24h,
+      market_cap_change_24h: this.market_cap_change_24h,
+      market_cap_change_percentage_24h: this.market_cap_change_percentage_24h,
+      circulating_supply: this.circulating_supply,
+      total_supply: this.total_supply,
+      max_supply: this.max_supply,
+      ath: this.ath,
+      ath_change_percentage: this.ath_change_percentage,
+      ath_date: this.ath_date,
+      atl: this.atl,
+      atl_change_percentage: this.atl_change_percentage,
+      atl_date: this.atl_date,
+      roi: this.roi,
+      last_updated: this.last_updated,
+    })
+  }
+
+  static deserialize(json) {
+    const {
+      id,
+      symbol,
+      name,
+      image,
+      current_price,
+      market_cap,
+      market_cap_rank,
+      fully_diluted_valuation,
+      total_volume,
+      high_24h,
+      low_24h,
+      price_change_24h,
+      price_change_percentage_24h,
+      market_cap_change_24h,
+      market_cap_change_percentage_24h,
+      circulating_supply,
+      total_supply,
+      max_supply,
+      ath,
+      ath_change_percentage,
+      ath_date,
+      atl,
+      atl_change_percentage,
+      atl_date,
+      roi,
+      last_updated,
+    } = JSON.parse(json)
+
+    return new Coin(
+      id,
+      symbol,
+      name,
+      image,
+      current_price,
+      market_cap,
+      market_cap_rank,
+      fully_diluted_valuation,
+      total_volume,
+      high_24h,
+      low_24h,
+      price_change_24h,
+      price_change_percentage_24h,
+      market_cap_change_24h,
+      market_cap_change_percentage_24h,
+      circulating_supply,
+      total_supply,
+      max_supply,
+      ath,
+      ath_change_percentage,
+      ath_date,
+      atl,
+      atl_change_percentage,
+      atl_date,
+      roi,
+      last_updated
+    )
+  }
+}
+
+module.exports = { Coin }
