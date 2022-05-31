00f63bd35495f522579ed54afa8f03b265625879
Tobias Leinss <caruso33@web.de>
2022-05-31 21:10:05 +0200

    Setup contracts

--- nader/4_nft_marketplace/hardhat.config.js
+++ nader/4_nft_marketplace/hardhat.config.js
@@ -1,21 +1,18 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require("dotenv").config()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
-};
+  hardhat: {
+    chainId: 1337,
+  },
+  mumbai: {
+    url: process.env.POLYGON_MUMBAI,
+    accounts: [process.env.PRIVATE_KEY]
+  },
+  mainnet: {
+    url: process.env.POLYGON_MAIN,
+    accounts: [process.env.PRIVATE_KEY]
+  },
+}
