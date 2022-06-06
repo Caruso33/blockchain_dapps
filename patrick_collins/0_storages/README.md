36ef27186faff3ff6a72f803610811ff559a224f
Tobias Leinss <caruso33@web.de>
2022-06-06 13:58:16 +0200

    Add collins storage contracts and mumbai deployment

---
+++ patrick_collins/0_storages/README.md
@@ -0,0 +1,5 @@
+# Address
+
+## Polygon
+
+StorageFactory deployed to: 0x10d7526150f4134d9B6631c8C4A6d812a91dFfA7
---
+++ patrick_collins/0_storages/contracts/SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    event PersonAdded(string name, uint256 favoriteNumber, uint256 personCount);
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+
+        emit PersonAdded(_name, _favoriteNumber, people.length);
+    }
+}
