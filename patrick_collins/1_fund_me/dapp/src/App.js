3b671b7973d30456f5972937316ecda15540655f
Tobias Leinss <caruso33@web.de>
2022-06-07 12:37:58 +0200

    wagmi try

--- patrick_collins/1_fund_me/dapp/src/App.js
+++ patrick_collins/1_fund_me/dapp/src/App.js
@@ -1,5 +1,40 @@
+import {
+  chain,
+  useAccount,
+  useConnect,
+  useNetwork,
+  useBalance,
+  useEnsName,
+} from "wagmi"
+import { InjectedConnector } from "wagmi/connectors/injected"
 function App() {
-  return <div>App </div>
+  const { connect } = useConnect({
+    connector: new InjectedConnector({
+      // connector: new MetaMaskConnector({
+      chains: [chain.localhost],
+    }),
+  })
+  const network = useNetwork({
+    chainId: 1337,
+  })
+  console.dir("network", network)
+
+  const { data: account } = useAccount()
+  console.dir("account", account)
+
+  const { data: ensName } = useEnsName({ address: account?.address })
+  console.log("ensName", ensName)
+
+  const { data: balance } = useBalance({
+    addressOrName: account?.address,
+  })
+  console.dir("balance", balance)
+
+  console.log("value", balance?.value?.toString())
+
+  if (account) return <div>Connected to {ensName ?? account?.address}</div>
+
+  return <button onClick={() => connect()}>Connect Wallet</button>
 }
 
 export default App
--- patrick_collins/1_fund_me/dapp/src/index.js
+++ patrick_collins/1_fund_me/dapp/src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { providers } from "ethers"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { chain, createClient, WagmiConfig } from "wagmi"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = createClient({
+  autoConnect: true,
+  provider({ chainId }) {
+    // Use JsonRpcProvider for localhost
+    console.log("chainId", chainId)
+    console.log("localhost.id", chain.localhost.id)
+
+    if (chainId === 1337 || chainId === 31337 || chainId === chain.localhost.id)
+      return new providers.JsonRpcProvider()
+
+    return new providers.AlchemyProvider(chainId)
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
