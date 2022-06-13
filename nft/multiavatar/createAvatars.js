8d21a4550bbefc5f23bfc0c98c08981e391e1705
Tobias Leinss <caruso33@web.de>
2022-06-13 13:43:06 +0200

    Add faker

--- nft/multiavatar/createAvatars.js
+++ nft/multiavatar/createAvatars.js
@@ -1,7 +1,7 @@
+import { faker } from "@faker-js/faker"
 import multiavatar from "@multiavatar/multiavatar/esm"
-import svg2img from "svg2img"
 import fs from "fs"
-import wordList from "./BIP-0039-word-list.js"
+import svg2img from "svg2img"
 
 function createSvg(string = "Binx Bond") {
   const svgCode = multiavatar(string)
@@ -21,29 +21,12 @@ async function saveSvgToImg(svgCode, outputPath) {
     })
   })
 }
-function getRandomWord() {
-  const randomWord = wordList[Math.floor(Math.random() * wordList.length)]
-  return randomWord
-}
-
-function generateRandomAvatars() {
-  const wordLength = Math.floor(Math.random() * 10) + 1
-
-  const avatarWords = []
-
-  for (let i = 0; i < wordLength - 1; i++) {
-    const randomWord = getRandomWord()
-    avatarWords.push(randomWord)
-  }
-
-  return avatarWords.join(" ")
-}
 
 async function main(collectionName = "output", numberOfAvatars = 1) {
   const promises = []
 
   for (let i = 0; i < numberOfAvatars; i++) {
-    const avatarString = generateRandomAvatars()
+    const avatarString = faker.name.findName()
 
     const svgCode = createSvg(avatarString)
 
