24d1b0b838159d7cda77fb14f4193acf209e399d
Tobias Leinss <caruso33@web.de>
2022-06-17 17:00:50 +0200

    WIP file upload

--- ipfs-filecoin/nft-storage/put-files.js
+++ ipfs-filecoin/nft-storage/put-files.js
@@ -62,20 +62,20 @@ async function fileFromPath(filePath) {
 async function main() {
   const name = "TEST",
     description = "TEST DESCRIPTION",
-    imagePath = "./multiavatar.svg"
+    imagePath = "multiavatar.svg"
 
   //   const imageSvgPath = `<!DOCTYPE html><svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-  //     <path d="M 10,30
-  //              A 20,20 0,0,1 50,30
-  //              A 20,20 0,0,1 90,30
-  //              Q 90,60 50,90
-  //              Q 10,60 10,30 z"/>
-  //   </svg>
-  //   `
+  //       <path d="M 10,30
+  //                A 20,20 0,0,1 50,30
+  //                A 20,20 0,0,1 90,30
+  //                Q 90,60 50,90
+  //                Q 10,60 10,30 z"/>
+  //     </svg>
+  //     `
 
   //   const svgDom = new JSDOM(imageSvgPath)
 
-  //   const element = svgDom.window.document.querySelector("svg")
+  //   const element = svgDom.window.document.getElementsByTagName("svg")[0]
   //   console.log(element)
 
   //   const dataUri = await saveSvgAsPng.svgAsDataUri(element)
