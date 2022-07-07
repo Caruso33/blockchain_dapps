5452a29573d11db105f813442d3a57b7c1d85da4
Tobias Leinss <caruso33@web.de>
2022-07-07 16:14:39 +0200

    Switch to hardhat

---
+++ patrick_collins/0_storages/scripts/deploy.ts
@@ -0,0 +1,42 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+import fs from "fs";
+import path from "path";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const StorageFactory = await ethers.getContractFactory("StorageFactory");
+
+  const storageFactory = await StorageFactory.deploy();
+  await storageFactory.deployed();
+
+  console.log("StorageFactory deployed to: ", storageFactory.address);
+
+  const config = {
+    address: storageFactory.address,
+  };
+
+  fs.writeFileSync(
+    path.join(__dirname, "/../dapp/public/config.json"),
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
