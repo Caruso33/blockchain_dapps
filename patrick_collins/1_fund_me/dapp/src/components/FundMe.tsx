242c4a3374d686161294cfa2a6e25ae18f2dc5e7
Tobias Leinss <caruso33@web.de>
2022-07-12 17:29:54 +0200

    Dapps does what's expected

--- patrick_collins/1_fund_me/dapp/src/components/FundMe.tsx
+++ patrick_collins/1_fund_me/dapp/src/components/FundMe.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react"
 import deployment from "../utils/deployment.json"
 import Spinner from "./Spinner.tsx"
-import { useAccount, useNetwork, useSigner } from "wagmi"
+import { useAccount, useNetwork, useProvider, useSigner } from "wagmi"
 import { Contract, ethers } from "ethers"
 
 export default function FundMe() {
   const [contract, setContract] = useState<Contract | null>(null)
 
+  const [fundingBalance, setFundingBalance] = useState("")
   const [funders, setFunders] = useState([])
+  const [funder, setFunder] = useState("")
+
   const [fundAmount, setFundAmount] = useState("")
-  const [version, setVersion] = useState("")
+  const [funderIndex, setFunderIndex] = useState("")
 
   const [isLoading, setIsLoading] = useState(false)
 
   const { isConnected } = useAccount()
   const { data: signer } = useSigner()
   const { chain } = useNetwork()
+  const provider = useProvider()
 
   useEffect(() => {
     if (!signer || !chain?.id) {
@@ -33,10 +37,30 @@ export default function FundMe() {
     setContract(contract)
   }, [chain, signer])
 
+  async function getFundingBalance() {
+    if (!contract || !provider) {
+      return
+    }
+
+    setIsLoading(true)
+
+    const balance = await provider.getBalance(contract.address)
+    console.log("Balance: ", ethers.utils.formatEther(balance.toString()))
+    setFundingBalance(ethers.utils.formatEther(balance.toString()))
+
+    setIsLoading(false)
+
+    return balance
+  }
+
   async function getFunders() {
+    if (!contract) {
+      return
+    }
+
     setIsLoading(true)
     try {
-      const funders = await fundMe?.getFunders()
+      const funders = await contract?.getFunders()
       console.log("Funders: ", funders)
       setFunders(funders)
     } finally {
@@ -44,35 +68,53 @@ export default function FundMe() {
     }
   }
 
-  async function fund() {
+  async function getFunder() {
+    if (!contract || !funderIndex || isNaN(+funderIndex)) {
+      return
+    }
+
     setIsLoading(true)
     try {
-      const amount = ethers.utils.parseEther(fundAmount)
-      console.dir(fundAmount, amount.toString())
-
-      const tx = await fundMe?.fund({ value: amount })
-      await tx.wait()
+      const funder = await contract?.getFunder(+funderIndex)
+      console.log("Funder: ", funder)
+      setFunder(funder)
     } finally {
       setIsLoading(false)
     }
   }
 
-  //   async function getVersion() {
-  //     setIsLoading(true)
-  //     try {
-  //       const version = await fundMe?.getVersion()
-  //       console.log("Version: ", version)
-  //       setVersion(version)
-  //     } finally {
-  //       setIsLoading(false)
-  //     }
-  //   }
+  async function fund() {
+    if (!contract || !fundAmount || isNaN(+fundAmount)) {
+      return
+    }
 
-  async function withdraw() {
     setIsLoading(true)
     try {
-      const tx = await fundMe?.withdraw()
+      const amount = ethers.utils.parseEther(fundAmount)
+      console.log(fundAmount, amount.toString())
+
+      const tx = await contract?.fund({ value: amount })
       await tx.wait()
+      console.log("Funded")
+      getFundingBalance()
+      getFunders()
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function withdraw() {
+    if (!contract) {
+      return
+    }
+
+    setIsLoading(true)
+    try {
+      const tx = await contract?.withdraw()
+      await tx.wait()
+      console.log("Withdrawn")
+      getFundingBalance()
+      getFunders()
     } finally {
       setIsLoading(false)
     }
@@ -81,11 +123,27 @@ export default function FundMe() {
   return (
     isConnected && (
       <div>
+        <h1>FundMe</h1>
+        <h2>Funding Balance</h2>
+        <button onClick={getFundingBalance}>
+          {isLoading ? <Spinner /> : "Get Funding Balance"}
+        </button>
+        <pre>{fundingBalance}</pre>
         <h2>Get funders</h2>
         <button onClick={getFunders}>
           {isLoading ? <Spinner /> : "Get funders"}
         </button>
         <pre>Funders: {funders.join(", ")}</pre>
+        <h2>Get funder</h2>
+        <input
+          type="text"
+          value={funderIndex}
+          onChange={(e) => setFunderIndex(e.target.value)}
+        />
+        <button onClick={() => getFunder(fundAmount)}>
+          {isLoading ? <Spinner /> : "Get funder"}
+        </button>
+        <pre>Funder: {funder}</pre>
         <h2>Fund</h2>
         <input
           type="number"
@@ -93,14 +151,8 @@ export default function FundMe() {
           value={fundAmount}
           onChange={(e) => setFundAmount(e.target.value)}
         />
-        <button onClick={fund}>
-          {isLoading ? <Spinner /> : "Save storage"}
-        </button>
-        {/* <h2>Get Version</h2>
-        <button onClick={getVersion}>
-          {isLoading ? <Spinner /> : "Get Version"}
-        </button>
-        <pre>Version: {version}</pre> */}
+        <button onClick={fund}>{isLoading ? <Spinner /> : "Fund"}</button>
+
         <h2>Withdraw</h2>
         <button onClick={withdraw}>
           {isLoading ? <Spinner /> : "Withdraw"}
