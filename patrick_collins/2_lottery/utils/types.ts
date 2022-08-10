0ce7e2568a334914e75cc2dbbc939bba70b59c05
Tobias Leinss <caruso33@web.de>
2022-08-10 12:15:32 +0200

    Add lottery frontend starter

--- patrick_collins/2_lottery/utils/types.ts
+++
@@ -1,9 +0,0 @@
-type HHDeploy {
-    
-        getNamedAccounts: () => Promise<{ [name: string]: string }>,
-        deployments: {
-            deploy: (contractName: string, options?: any) => Promise<any>
-            log: (message: string) => void
-        }
-    
-}
