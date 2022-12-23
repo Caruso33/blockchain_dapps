f1a5a0e3c03a0014a3a3fc6c363b1f29a018809a
Tobias Leinss <tobias.leinss@mway.io>
2022-12-23 16:02:14 +0100

    further with forge

--- hello_foundry/README.md
+++ hello_foundry/README.md
@@ -13,3 +13,5 @@
 `forge update lib/solmate` - update dep
 
 `forge remove solmate` - remove dep
+
+`forge create --rpc-url <your_rpc_url> --private-key <your_private_key> src/Counter.sol:Counter --constructor-args "0" --verify` - deploy contract
--- hello_foundry/src/Counter.sol
+++ hello_foundry/src/Counter.sol
@@ -6,11 +6,20 @@ import "solmate-utils/LibString.sol";
 contract Counter {
     uint256 public number;
 
+    event IncrementCounter(uint256 newNumber);
+    event SetNewNumber(uint256 newNumber);
+
+    constructor(uint256 startNumber) {
+        number = startNumber;
+    }
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
     function increment() public {
         number++;
+
+        emit IncrementCounter(number);
     }
 }
